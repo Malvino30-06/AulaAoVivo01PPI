08b84efce87595f2ca25e3fcea798f7fda64a236
Malvino30-06 <malvinogarcia2@gmail.com>
2026-02-16 19:17:51 -0300

    Implementando uma tabuada para alteração dinâmica.

--- index.js
+++ index.js
@@ -45,6 +45,53 @@ app.get("/horaAtual", (req, res) => {
         </html>`);
 });
 
+// criar um método que aceita parâmetros
+
+app.get("/tabuada", (req, res) => {
+  // tabuada de qual número e até qual sequência?
+  const numero = parseInt(req.query.numero);
+  const sequencia = parseInt(req.query.sequencia);
+
+  if (!numero || !sequencia) {
+    res.send(`<!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" concent="width=device-width, initial-scale=1.0">
+            <title>Tabuada</title>
+        </head>
+        <body>
+            <h1>Olá, mundo!</h1>
+            <h2>Por favor informe o número e a sequência na URL</h2>
+        </body>
+        </html>`);
+  } else {
+    res.write(`<!DOCTYPE html>
+        <html lang="pt-br">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" concent="width=device-width, initial-scale=1.0">
+            <title>Tabuada</title>
+        </head>
+        <body>
+            <h2>Tabuada do ${numero} até a sequência ${sequencia}.</h2>
+
+            <ul> 
+        `);
+    for (let i = 0; i <= sequencia; i++) {
+      res.write(`
+                <li>${numero} X ${i} = ${numero * i} </li>`);
+    }
+
+    res.write(`
+            </ul>
+        </body>
+        </html>`);
+    res.end(); // finaliza e envia
+  }
+  console.log("requisição tabuada");
+});
+
 app.listen(porta, host, () => {
   console.log(`Servidor escutando em http://${host}:${porta}`);
 });
